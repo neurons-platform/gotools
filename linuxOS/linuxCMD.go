8831f6fb242e1b91bbcb7f1af6e4ddd1f4fbef91
jingminglang <cdjingminglang@jd.com>
2018-10-08 17:04:45 +0800

    add: linuxCmd ExeCmd

--- linuxOS/linuxCMD.go
+++ linuxOS/linuxCMD.go
@@ -7,19 +7,32 @@ type CmdArg struct {
 }
 
 type LinuxCMD struct {
+	// 命令名
 	Name       string   `yaml:"Name"`
+	// 命令所在位置 fullpath
 	Path       string   `yaml:"Path"`
+	// 执行命令的程序
+	ExeCmd string `yaml:"ExeCmd"`
+	// 日志位置
 	LogPath    string    `yaml:"LogPath"`
+	// 命令版本
 	Version    string   `yaml:"Version"`
+	// 使用方法
 	UseCase    []CmdArg `yaml:"UseCase"`
+	// 安装方法
 	InstallCMD string   `yaml:"InstallCMD"`
+	// 描述
 	Desc       string   `yaml:"Desc"`
 }
 
 func (c LinuxCMD) FindUseCase(useCase string) func(string) string {
 	for _, v := range c.UseCase {
 		return func(str string) string {
-			return c.Path + " " + v.Args + " " + str
+			if len(c.ExeCmd) >0 {
+				return c.ExeCmd + " " +  c.Path + " " + v.Args + " " + str
+			} else {
+				return c.Path + " " + v.Args + " " + str
+			}
 		}
 	}
 
